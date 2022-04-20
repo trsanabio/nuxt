939f0a41576bbb37b2c5721a3bb55a215fcf65e5
Thyago Sanabio <trsanabio@gmail.com>
2022-04-20 15:21:10 -0300

    Aula 7 - asyncData

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
         <p>The HTML spec requires that blockquote attribution be placed outside the blockquote. When providing attribution, wrap your blockquote in a figure and use a figcaption or a block level element (e.g., p) with the .blockquote-footer class. Be sure to wrap the name of the source work in cite as well.</p>
+
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products" :key="product.id">
+                <th scope="row">{{ product.id }}</th>
+                <td>{{ product.title }}</td>
+                </tr>
+            </tbody>
+        </table>
+
     </div>
 </template>
 
@@ -14,6 +30,24 @@
             return{
 
             };
+        },
+
+        async asyncData( {$axios} ){
+            const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts?_limit=3')
+            // console.log(context);
+            // await new Promise ( (resolve) => {
+            //     setTimeout( () => {
+            //         resolve()
+            //         console.log('promise resolvida');
+            //     }, 3000)
+            // })
+            return{
+                products,
+                // products: [
+                //     {name: 'product-a'},
+                //     {name: 'product-b'},
+                // ],
+            }
         },
 
         methods: {},
